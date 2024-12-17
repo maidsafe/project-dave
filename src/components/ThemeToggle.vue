1c77e0045939e7920778b68d9664b827c39f1bb6
Danny Beck <dlc.beck@gmail.com>
2024-12-16 17:04:34 -0800

    adds darkmode using tailwind

---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="toggleTheme" class="p-2 rounded-md border dark:border-white border-black">
+    {{ isDark ? '🌙' : '☀️' }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from '../composables/useTheme'
+
+const { isDark, toggleTheme } = useTheme()
+</script>
