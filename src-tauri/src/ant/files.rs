4bf3d34bfe3cb7bf00d2cd88687baadf385de0a1
Warm Beer <mickvd99@gmail.com>
2024-12-17 16:25:13 +0100

    chore: upload files changes wip

--- src-tauri/src/ant/files.rs
+++ src-tauri/src/ant/files.rs
@@ -1,7 +1,10 @@
 use std::path::{PathBuf};
 use autonomi::{Bytes, Chunk};
+use autonomi::client::data::DataMapChunk;
+use autonomi::client::files::archive::Metadata;
 use serde::Deserialize;
 use tauri::{AppHandle, Emitter, State};
+use crate::ant::client::client;
 use crate::ant::payments::PaymentOrderManager;
 
 #[derive(Deserialize)]
@@ -12,6 +15,7 @@ pub struct File {
 
 pub struct FileEncrypted {
     name: String,
+    metadata: Metadata,
     datamap: Chunk,
     chunks: Vec<Chunk>
 }
@@ -21,18 +25,33 @@ pub async fn read_file_to_bytes(file_path: PathBuf) -> Bytes {
 }
 
 pub async fn upload_files(app: AppHandle, files: Vec<File>, archive_name: &str, payment_orders: State<'_, PaymentOrderManager>) {
+    let client = client().await;
+
     let mut encrypted_files = vec![];
 
     for file in files {
         let bytes: Bytes = read_file_to_bytes(file.path).await;
-
+        let metadata = Metadata::new_with_size(bytes.len() as u64);
         let (datamap, chunks) = autonomi::self_encryption::encrypt(bytes).unwrap();
 
         encrypted_files.push(FileEncrypted {
             name: file.name,
+            metadata,
             datamap,
             chunks,
         })
+    }
+
+    let mut private_archive = autonomi::PrivateArchive::new();
+
+    // let mut store_quotes = vec![];
+
+    for file in &encrypted_files {
+        private_archive.add_file(PathBuf::from(&file.name), DataMapChunk::from(file.datamap.clone()), file.metadata.clone());
+
+        // let chunk_addresses = file.chunks.iter().map(|chunk| chunk.address).collect();
+
+        // client.get_store_quotes()
     }
 
     // build archive
