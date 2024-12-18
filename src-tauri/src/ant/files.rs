67e3ad73bfa02763832f199fdce232e46cfd96b2
Benno Zeeman <bzeeman@live.nl>
2024-12-18 10:54:16 +0100

    feat: get archive files from user data

--- src-tauri/src/ant/files.rs
+++ src-tauri/src/ant/files.rs
@@ -2,6 +2,8 @@ use crate::ant::client::client;
 use crate::ant::payments::PaymentOrderManager;
 use autonomi::client::data::DataMapChunk;
 use autonomi::client::files::archive::Metadata;
+use autonomi::client::vault::user_data::UserDataVaultGetError;
+use autonomi::client::vault::VaultSecretKey;
 use autonomi::{Bytes, Chunk};
 use serde::Deserialize;
 use std::path::PathBuf;
@@ -78,4 +80,102 @@ pub async fn upload_files(
     // request payment of quotes to frontend
     // upload chunks and archive
     todo!()
+}
+
+#[derive(Deserialize)]
+pub struct PrivateFileFromVault {
+    paths: PrivateFileFromVaultPath,
+    size: u64,
+    date_created: u64,
+    date_modified: u64,
+    date_uploaded: u64,
+    private_data_access: DataMapChunk,
+}
+
+#[derive(Deserialize)]
+pub struct PrivateFileFromVaultPath {
+    local: String,
+}
+
+pub async fn get_private_files_from_vault(
+    secret_key: &VaultSecretKey,
+) -> Result<Vec<PrivateFileFromVault>, UserDataVaultGetError> {
+    let client = client().await;
+
+    // Fetch user data
+    let user_data = client.get_user_data_from_vault(secret_key).await?;
+
+    let mut files: Vec<PrivateFileFromVault> = vec![];
+
+    for (data_map, name) in user_data.private_file_archives {
+        let archive_name = name.replace(",", "-").replace("/", "-").replace(" ", "");
+
+        // Get private archive
+        let archive = client.archive_get(data_map).await?;
+
+        for (filename, (data_map, metadata)) in archive.map() {
+            let filepath = format!("{archive_name}/{}", filename.display());
+
+            let file = PrivateFileFromVault {
+                paths: PrivateFileFromVaultPath { local: filepath },
+                size: metadata.size,
+                date_created: metadata.created,
+                date_modified: metadata.modified,
+                date_uploaded: metadata.uploaded,
+                private_data_access: data_map.clone(),
+            };
+            files.push(file);
+        }
+    }
+
+    Ok(files)
+}
+
+#[derive(Deserialize)]
+pub struct PublicFileFromVault {
+    paths: PublicFileFromVaultPath,
+    size: u64,
+    date_created: u64,
+    date_modified: u64,
+    date_uploaded: u64,
+    data_addr: [u8; 32],
+}
+
+#[derive(Deserialize)]
+pub struct PublicFileFromVaultPath {
+    local: String,
+}
+
+pub async fn get_public_files_from_vault(
+    secret_key: &VaultSecretKey,
+) -> Result<Vec<PublicFileFromVault>, UserDataVaultGetError> {
+    let client = client().await;
+
+    // Fetch user data
+    let user_data = client.get_user_data_from_vault(secret_key).await?;
+
+    let mut files: Vec<PublicFileFromVault> = vec![];
+
+    for (archive_addr, name) in user_data.file_archives {
+        let archive_name = name.replace(",", "-").replace("/", "-").replace(" ", "");
+
+        // Get public archive
+        let archive = client.archive_get_public(archive_addr).await?;
+
+        for (filename, (data_addr, metadata)) in archive.map() {
+            let filepath = format!("{archive_name}/{}", filename.display());
+
+            let file = PublicFileFromVault {
+                paths: PublicFileFromVaultPath { local: filepath },
+                size: metadata.size,
+                date_created: metadata.created,
+                date_modified: metadata.modified,
+                date_uploaded: metadata.uploaded,
+                data_addr: data_addr.0,
+            };
+            files.push(file);
+        }
+    }
+
+    Ok(files)
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,7 @@
 use crate::ant::files::File;
 use crate::ant::payments::PaymentOrderManager;
+use ant::files::{PrivateFileFromVault, PublicFileFromVault};
+use autonomi::client::vault::VaultSecretKey;
 use tauri::{AppHandle, State};
 
 mod ant;
@@ -18,6 +20,24 @@ async fn upload_files(
 ) -> Result<(), ()> {
     ant::files::upload_files(app, files, "archive_name", payment_orders).await;
     Ok(())
+}
+
+#[tauri::command]
+pub async fn get_private_files_from_vault(
+    vault_key: &VaultSecretKey,
+) -> Result<Vec<PrivateFileFromVault>, ()> {
+    Ok(ant::files::get_private_files_from_vault(vault_key)
+        .await
+        .unwrap())
+}
+
+#[tauri::command]
+pub async fn get_public_files_from_vault(
+    vault_key: &VaultSecretKey,
+) -> Result<Vec<PublicFileFromVault>, ()> {
+    Ok(ant::files::get_public_files_from_vault(vault_key)
+        .await
+        .unwrap())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
