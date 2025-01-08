c4f130636360a1543afcc4ba66135d46168e724d
Warm Beer <mickvd99@gmail.com>
2025-01-08 14:13:13 +0100

    feat: use single shared `Client`

--- src-tauri/src/ant/client.rs
+++ src-tauri/src/ant/client.rs
@@ -1,5 +1,29 @@
-pub async fn client() -> autonomi::Client {
-    autonomi::client::Client::init()
-        .await
-        .expect("Failed to initialize client")
+use autonomi::client::ConnectError;
+use autonomi::Client;
+use tokio::sync::RwLock;
+
+#[derive(Default)]
+pub struct SharedClient {
+    client: RwLock<Option<Client>>,
+}
+
+impl SharedClient {
+    pub async fn connect(&self) -> Result<Client, ConnectError> {
+        let client = Client::init().await?;
+        *self.client.write().await = Some(client.clone());
+        Ok(client)
+    }
+
+    #[expect(dead_code)]
+    pub async fn disconnect(&self) {
+        *self.client.write().await = None;
+    }
+
+    pub async fn get_client(&self) -> Result<Client, ConnectError> {
+        if let Some(client) = self.client.read().await.clone() {
+            return Ok(client);
+        }
+
+        self.connect().await
+    }
 }
