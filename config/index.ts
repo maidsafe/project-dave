b978a472e4d4cc6995eddd6c12ce627429956441
Warm Beer <mickvd99@gmail.com>
2025-07-10 23:43:13 +0200

    fix: wallet disconnect for injected provider

--- config/index.ts
+++ config/index.ts
@@ -1,11 +1,17 @@
-import {WagmiAdapter} from '@reown/appkit-adapter-wagmi';
-import {arbitrum, type AppKitNetwork} from '@reown/appkit/networks';
+import {WagmiAdapter} from "@reown/appkit-adapter-wagmi";
+import {arbitrum} from "@reown/appkit/networks";
+import {injected} from "wagmi/connectors";
 
-export const projectId = 'c57e0bb001a4dc96b54b9ced656a3cb8';
+const connector = injected({
+    shimDisconnect: false,
+});
 
-export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [arbitrum];
+export const networks = [arbitrum];
+
+export const projectId = "1229cfd3cf7355433bc9ab7f4ea9e97e";
 
 export const wagmiAdapter = new WagmiAdapter({
     networks,
     projectId,
-});
+    connectors: [connector],
+})
